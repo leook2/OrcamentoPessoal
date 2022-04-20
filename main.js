ffb123da89ae0e56482210344ba3f56bc9524a79
Matheus Vinicius <96499491+AnaniasMv@users.noreply.github.com>
2022-04-20 08:32:36 -0300

    database

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-const express=require('express');
-const cors=require('cors');
-/* =======estou comentando o banco de dados pois a gente não tem um DEFINIDO por enquanto, e tbm para não dar conflito com o node na hora de iniciar
-const connection = require('./controllers/database');=========
-*/
-const bordyParser=require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const connection = require('./controllers/database');
+const bordyParser = require('body-parser');
 const { application } = require('express');
 const consign = require('consign');
 
-var app=express();
+var app = express();
 app.use(cors());
 
-/*=========estou comentando o banco de dados pois a gente não tem um DEFINIDO por enquanto, e tbm para não dar conflito com o node na hora de iniciar===========
 
 connection
     .authenticate()
-    .then(()=>{
+    .then(() => {
         console.log('Conectado ao Banco')
     })
-    .catch((erro)=>{
+    .catch((erro) => {
         console.log(erro)
     })
-*/
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bordyParser.urlencoded({extended: true})); 
-app.use(bordyParser.json()); 
+app.use(bordyParser.urlencoded({ extended: true }));
+app.use(bordyParser.json());
 
 
 consign()
     .include('./models')
     .include('./controllers/rotas')
     .into(app);
-const porta=8182;
-app.listen(porta, function(){
+const porta = 8182;
+app.listen(porta, function () {
     console.log(`Servidor rodando na porta ${porta}...`)
 })
