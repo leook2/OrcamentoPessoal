f1ab1fe339c7371d582b5c81a5712a197341662f
leonardoignacio <leonardoignacio@hotmail.com>
2022-05-03 11:11:30 -0300

    Rota para relatorio

--- controllers/rotas/transacao.js
+++ controllers/rotas/transacao.js
@@ -9,39 +9,6 @@ module.exports = function(app){
         query.findAll(res, 'transacao', Transacoes, 'idTransacao', 'DESC', CentrosCustos)
     })
 
-      app.get("/transacao/teste", (req, res) => {
-        /*Transacoes.sum('valorTransacao', {where: {idCentroCusto:1}})
-          .then(resp => {
-            console.log(resp)
-            res.json(resp)
-          })*/
-
-          Transacoes.findAll({
-            attributes:['idCentroCusto',
-            [Sequelize.fn('sum', Sequelize.col('valorTransacao')), 'SomaValor']
-          ],
-          include:[{model:CentrosCustos, attributes:{exclude:['idCentroCusto', 'idTipo']},
-                  include:[{model:Tipos}]}],
-          group:'idCentroCusto',
-          //raw:true
-          })
-          .then(cCusto=>{
-            Transacoes.findAll({
-              attributes:['idCentroCusto',
-              [Sequelize.fn('sum', Sequelize.col('valorTransacao')), 'SomaValor']
-            ],
-            include:[{model:CentrosCustos, attributes:{exclude:['idCentroCusto', 'nomeCentroCusto', 'idTipo']},
-                    include:[{model:Tipos}]}],
-            group:'nomeTipo',
-            //raw:true
-            })
-            .then(tipos =>{
-              console.log({centrosCustos:cCusto, tipos:tipos})
-              res.json({centrosCustos:cCusto, tipos:tipos})
-            })
-          })
-      });
-
       app.post('/transacao', (req, res)=>{
         let dados = {
           descricaoTransacao: req.body.descricao,
